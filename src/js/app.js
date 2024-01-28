f860c555dc0f97b4bbce63def17202cd2ddd585a
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-01-28 04:21:48 +0000

    10.0

--- src/js/app.js
+++ src/js/app.js
@@ -41,11 +41,15 @@ function render(variables = {}) {
     variables.country == null ? "" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
               variables.github == null ? "" : variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin == null ? "" : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://www.instagram.com/${
               variables.instagram == null ? "" : variables.instagram
             }/"><i class="fab fa-instagram"></i></a></li>
